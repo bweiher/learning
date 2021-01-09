f8e2a872a69f4485cfe79de360056a3487111511
ben weiher <bweiher@pm.me>
2021-01-08 16:38:49 -0800

    +

--- build_markdown.R
+++ build_markdown.R
@@ -1,49 +1,78 @@
 library(tidyverse)
 library(glue)
 
-urls <- paste0("links/", list.files("links") %>% 
-  str_subset("\\.URL$"))
-
+# ---  functions ----
 
 get_url <- function(url){
-  read.csv(url, stringsAsFactors = FALSE) %>% 
-  as_tibble() %>%
-  setNames("col") %>% 
-  filter(
-    str_detect(col, "URL")
-  ) %>% 
+   read.csv(url, stringsAsFactors = FALSE) %>% 
+    as_tibble() %>%
+    setNames("col") %>% 
+    filter(
+      str_detect(col, "URL")
+    ) %>% 
     mutate(col  = str_replace_all(col, "URL=", "")) %>% 
     pull(col)
 }
 
 get_url_safely <- safely(get_url, otherwise = NA_character_)
 
-df <- map_df(urls, function(x){
-    
-    print(x)
+
+
+# --- script --- 
+
+dirs <- list.files('links')
+dlist <- list()
+
+md <- map(seq_along(dirs), function(g){
+
   
-    fs::file_info(x) %>% 
-    as_tibble() %>% 
-    mutate(type=as.character(type),
-           path=as.character(path),
-           permissions=as.character(permissions),
-           size=as.character(size),
-           url =  get_url_safely(x)$result
-    )  
+  dir <- dirs[g]
+  path <- glue("links/{dir}/")
+  urls <- list.files(path) %>% str_subset("\\.URL$")
+  urls <- paste0(path,urls)
+  
+  
+  links_df <- map_df(urls, function(x){
+      fs::file_info(x) %>% 
+      as_tibble() %>% 
+      mutate(type=as.character(type),
+             path=as.character(path),
+             permissions=as.character(permissions),
+             size=as.character(size),
+             url =  get_url_safely(x)$result
+      )  
+    
+  })
+  
 
-}) %>% 
-  arrange(desc(modification_time))
+    links <- links_df %>% 
+    arrange(desc(modification_time)) %>% 
+    filter(!is.na(url)) %>% 
+    transmute(
+      name = str_replace_all(path, glue("^links/|{dirs_collapsed}"), ""), 
+      url) %>% 
+    mutate(
+      name = str_remove_all(name, "^/"),
+      x = glue("* [{name}]({url})") %>% 
+        str_remove_all('\\.URL')
+      ) %>% 
+    select(x) %>% 
+    pull()
+  
+  
+  c(
+    "\n",
+    paste0("### ", str_to_title(dir)),
+    links
+  )
+  
+})  
+  
 
-
-markdown <- df %>%
-  filter(!is.na(url)) %>% 
-  transmute(name = str_replace_all(path, "^links/", ""), url) %>% 
-  mutate(x = glue("* [{name}]({url})")) %>%
-  pull(x)
-
-
+  
+  
 c("## Learning \n",
-  as.character(markdown)
+  unlist(md) 
 ) -> lines
 
 
